1cf0e56c3ba5a60c904960300790407edd3d67d4
Omi <Omix1.ok@gmail.com>
2024-02-06 19:10:32 +0530

    + 🔨 : Nvim|Oil.nvim Default file manager then NetRw

--- nvim/lua/Fm/Oil.lua
+++ nvim/lua/Fm/Oil.lua
@@ -31,7 +31,7 @@ M = {
                 concealcursor = "n",
             },
             -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`
-            default_file_explorer = false,
+            default_file_explorer = true,
             -- Restore window options to previous values when leaving an oil buffer
             restore_win_options = true,
             -- Skip the confirmation popup for simple operations
